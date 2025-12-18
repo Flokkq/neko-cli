59e3e84ee568034b8423025f2905ff8e5582fd67
Benjamin Senekowisch <122978402+senbeb21@users.noreply.github.com>
2025-12-18 17:26:50 +0100

    chore: add type safety to nekoconfig

--- internal/config/nekoConfig.go
+++ internal/config/nekoConfig.go
@@ -3,11 +3,44 @@ package config
 /*
 @Author     Benjamin Senekowitsch
 @Contact    senekowitsch@nekoman.at
-@Since     17.12.2025
+@Since      17.12.2025
 */
 
+type ProjectType string
+type ReleaseType string
+
+const (
+	ProjectTypeFrontend ProjectType = "frontend"
+	ProjectTypeBackend  ProjectType = "backend"
+	ProjectTypeOther    ProjectType = "other"
+)
+
+const (
+	ReleaseTypeReleaseIt  ReleaseType = "release-it"
+	ReleaseTypeJReleaser  ReleaseType = "jreleaser"
+	ReleaseTypeGoReleaser ReleaseType = "goreleaser"
+)
+
 type NekoConfig struct {
-	ProjectType   string `json:"projectType"`
-	ReleaseSystem string `json:"releaseSystem"`
-	Version       string `json:"version"`
+	ProjectType   ProjectType `json:"projectType"`
+	ReleaseSystem ReleaseType `json:"releaseSystem"`
+	Version       string      `json:"version"`
+}
+
+func (p ProjectType) IsValid() bool {
+	switch p {
+	case ProjectTypeFrontend, ProjectTypeBackend, ProjectTypeOther:
+		return true
+	default:
+		return false
+	}
+}
+
+func (r ReleaseType) IsValid() bool {
+	switch r {
+	case ReleaseTypeReleaseIt, ReleaseTypeJReleaser, ReleaseTypeGoReleaser:
+		return true
+	default:
+		return false
+	}
 }
