5eee052824743bfc0164af561067de70115e7d35
Benjamin Senekowisch <122978402+senbeb21@users.noreply.github.com>
2025-12-20 16:43:00 +0100

    feature: add log colors in verbose output

--- internal/log/verbose.go
+++ internal/log/verbose.go
@@ -2,6 +2,14 @@ package log
 
 import "fmt"
 
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBold   = "\033[1m"
+)
+
 var Verbose = false
 
 func V(msg string, args ...interface{}) {
